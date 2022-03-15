251f2384a7e74080ab385bcb34c090200ab8fc0e
HerryRedstone <thachlan@outlook.com>
2022-03-15 13:31:00 +0700

    Add files via upload

--- deploy/00-deploy.js
+++ deploy/00-deploy.js
@@ -2,14 +2,6 @@
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
-    const contract = await deploy('AstarWeb3Domains', {
-      from: deployer,
-      args: [],
-      log: true
-    });
-	
 	console.log("Deploying:", contract.address);
-	
-	return contract.address;
 };
-module.exports.tags = ['AstarWeb3Domains'];
+module.exports.tags = ['AstarWeb3Domain'];
--- scripts/AstarWeb3Domain.js
+++ scripts/AstarWeb3Domain.js
@@ -4,9 +4,10 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+//const hredeploy = require("hardhat-deploy");
 const express = require('express');
 const app = express();
-const port = 8888;
+const port = 9000;
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -27,7 +28,7 @@ async function main() {
   const contract = await deploy('AstarWeb3Domain', {
       from: deployer,
       args: [],
-      log: true
+     log: true
   });
 }
 
@@ -36,6 +37,10 @@ main().then(() => process.exit(0))
 		console.error(error);
 		process.exit(1);
 	  });;
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+
 
 app.get('/deploy', (req, res) => {
 	main()
